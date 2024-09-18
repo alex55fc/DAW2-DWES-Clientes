cc2445012912dd2d2344d2b2a4666dd5ec18d209
alexa <alexander.55.fc@gmail.com>
2024-09-18 16:58:05 +0200

    I made my own dashboard to practice

--- menu.php
+++ menu.php
@@ -4,26 +4,9 @@
           <h5 class="offcanvas-title" id="sidebarMenuLabel">Company name</h5>
           <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
         </div>
+          
         <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
           <ul class="nav flex-column">
-            <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="#">
-                <svg class="bi"><use xlink:href="#house-fill"/></svg>
-                Dashboard
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
-                <svg class="bi"><use xlink:href="#file-earmark"/></svg>
-                Orders
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
-                <svg class="bi"><use xlink:href="#cart"/></svg>
-                Products
-              </a>
-            </li>
             <li class="nav-item">
               <a class="nav-link d-flex align-items-center gap-2" href="clientes.php">
                 <svg class="bi"><use xlink:href="#people"/></svg>
@@ -31,15 +14,15 @@
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
+              <a class="nav-link d-flex align-items-center gap-2" href="usuarios/usuarios.php">
                 <svg class="bi"><use xlink:href="#graph-up"/></svg>
-                Reports
+                Usuarios
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
-                <svg class="bi"><use xlink:href="#puzzle"/></svg>
-                Integrations
+          <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2" href="mi%20dashboard/midashboard.php">
+                <svg class="bi"><use xlink:href="#graph-up"/></svg>
+                Mi dashboard
               </a>
             </li>
           </ul>
